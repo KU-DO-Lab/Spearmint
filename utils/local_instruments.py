07f3a801f8ef1d7c6fbfcd88e5e724f451fd4d68
Grant <mumbo1002@gmail.com>
2024-09-27 15:42:04 -0500

    Lakeshore 336 appears in instruments list now

--- utils/local_instruments.py
+++ utils/local_instruments.py
@@ -4,6 +4,7 @@ from qcodes.instrument_drivers.american_magnetics.AMI430 import AMI430
 from qcodes.instrument_drivers.stanford_research.SR860 import SR860
 from qcodes.instrument_drivers.stanford_research.SR830 import SR830
 from qcodes.instrument_drivers.tektronix.Keithley_2450 import Keithley2450
+from qcodes.instrument_drivers.Lakeshore import LakeshoreModel336
 
 # To add an instrument, import the driver then add it to our instrument
 # dictionary with the name as the key, and the class as the value
@@ -12,4 +13,5 @@ LOCAL_INSTRUMENTS = {'NI DAQ': Daq,
                      'AMI430': AMI430,
                      'SR860': SR860,
                      'SR830': SR830,
-                     'Keithley2450': Keithley2450}
+                     'Keithley2450': Keithley2450,
+                     'Lakeshore336': LakeshoreModel336}
